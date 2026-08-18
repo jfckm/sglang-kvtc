53483d9a09739db6cb4856da2372127722609c70
Jan Musial <jfckm@pm.me>
2026-08-18 12:00:32 +0200

    kvtc: Make compressed_dtype = hicache_dtype
    Signed-off-by: Jan Musial <jfckm@pm.me>

--- python/sglang/srt/mem_cache/memory_pool_host.py
+++ python/sglang/srt/mem_cache/memory_pool_host.py
@@ -3300,7 +3300,7 @@ class NPUMHATokenToKVPoolCompressed(HostKVCache):
         self.page_size = page_size
         self.device = "cpu"
         self.dtype = device_pool.store_dtype
-        self.compressed_dtype = torch.float32
+        self.compressed_dtype = self.dtype
         self.rotary_emb = rotary_emb
         self.tp_rank = tp_rank
         self.pp_rank = pp_rank
